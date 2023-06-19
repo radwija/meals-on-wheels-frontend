6e0c457f835f42f972da6f9174bb2b530b6c627d
aguswirayasa <aguswirayasa08@gmail.com>
2023-06-19 21:46:17 +0800

    creating UI for registration page and updating color scheme

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import Profile from "./pages/Profile";
 import AboutUs from "./pages/AboutUs";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Registration />
     </div>
   );
 }
