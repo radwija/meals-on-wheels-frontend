ad6afabe97fe65095a9bc86f150b2ef98e47ed37
radwija <115165737+radwija@users.noreply.github.com>
2023-06-20 01:55:46 +0800

    footer milestone 1_v2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,16 @@ import mow_logo from "../assets/mow_logo.png"
 export const Footer = () => {
     return (
         <footer className='bg-footer pt-10 pb-10'>
-            <div className='container mx-auto grid grid-cols-3 gap-4 '>
-                <ul className='flex flex-wrap items-center mt-3 justify-around'>
+            <div className='container mx-auto grid grid-cols-5 gap-10 '>
+                <ul className='flex flex-wrap items-center mt-3 col-span-2 justify-end gap-20'>
                     <li>Donate</li>
                     <li>Partnerships</li>
                     <li>About Us</li>
                 </ul>
-                <img src={mow_logo} className='h-52 mx-auto' alt="" />
-                <ul className='flex flex-wrap items-center mt-3 justify-around'>
+                <img src={mow_logo} className=' col-span-1 mx-auto' alt="" />
+                <ul className='flex flex-wrap items-center mt-3  col-span-2 justify-start gap-20'>
                     <li>Contact Us</li>
                     <li>Terms and Conditions</li>
-                    <li>Link</li>
                 </ul>
             </div>
             <hr className='mr-20 ml-20 mb-5 h-0.5 bg-black' />
