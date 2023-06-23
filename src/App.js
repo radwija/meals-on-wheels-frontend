95d0d805f938f4dbc473566861c8a051c3361dab
aguswirayasa <aguswirayasa08@gmail.com>
2023-06-23 14:18:55 +0800

    implemting partnership logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Registration from "./pages/Registration";
 import { Footer } from "./components/Footer";
+import Partnership from "./pages/Partnership";
 
 function App() {
   return (
     <div className="App">
-      <Registration />
+      <Partnership />
       <Footer />
     </div>
   );
