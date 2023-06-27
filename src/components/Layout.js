04626428ad9ed339b93749b741468db60ff88317
radwija <115165737+radwija@users.noreply.github.com>
2023-06-27 13:45:51 +0800

    creating navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Footer } from "./Footer";
 import { Outlet } from "react-router";
 import { LandingPage } from "../pages/LandingPage";
+import { Navbar } from "./Navbar";
 const Layout = (props) => {
   return (
     <div>
+      <Navbar />
       {/* <Outlet /> */}
-      <div className="min-h-screen">
+      <div className="min-h-screen pt-[65px]">
         {props.children}
       </div>
       <Footer />
