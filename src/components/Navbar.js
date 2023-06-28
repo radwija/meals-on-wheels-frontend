69fe9b926a1e4bae2b995450d65bac8a471c1c0d
radwija <115165737+radwija@users.noreply.github.com>
2023-06-28 15:42:30 +0800

    hiding user menu after logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -70,6 +70,7 @@ export const Navbar = () => {
                     <img src={mow_navbar_logo} className="h-8 mr-3" alt="Flowbite Logo" />
                 </Link>
                 <div className="flex items-center md:order-2">
+                    <Link onClick={() => handleSignOut()} className="block mr-0 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</Link>
                     {
                         !auth() ?
                             <div>
@@ -107,23 +108,27 @@ export const Navbar = () => {
                             </button>
                     }
                     {/* <!-- Dropdown menu --> */}
-                    <div id="user-dropdown" className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" >
-                        <div className="px-4 py-3">
-                            <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-                            <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-                        </div>
-                        <ul className="py-2" aria-labelledby="user-menu-button">
-                            <li>
-                                <Link to={"#"} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</Link>
-                            </li>
-                            <li>
-                                <Link to={"#"} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</Link>
-                            </li>
-                            <li>
-                                <Link onClick={() => handleSignOut()} className="block mr-0 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</Link>
-                            </li>
-                        </ul>
-                    </div>
+                    {
+                        auth() ?
+                            <div id="user-dropdown" className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" >
+                                <div className="px-4 py-3">
+                                    <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                                    <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                                </div>
+                                <ul className="py-2" aria-labelledby="user-menu-button">
+                                    <li>
+                                        <Link to={"#"} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"#"} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</Link>
+                                    </li>
+                                    <li>
+                                        <Link onClick={() => handleSignOut()} className="block mr-0 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            : <></>
+                    }
                     <button id="triggerEl" data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
