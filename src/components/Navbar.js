2965db0071b0f90561f21235db5b1d850e75843c
radwija <115165737+radwija@users.noreply.github.com>
2023-06-28 11:56:15 +0800

    styling login and register button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,10 +34,11 @@ export const Navbar = () => {
     const location = useLocation();
     return (
         <nav className="fixed mx-auto w-full top-0 z-20 bg-white border-b border-gray-200">
-            <div div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4" >
-                <Link href="https://flowbite.com/" className="flex items-center">
+            <div div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4" >
+                <Link to={"/"} className="flex items-center">
                     <img src={mow_navbar_logo} className="h-8" alt="MOW Logo Navbar" />
                 </Link>
+
                 <div className="flex items-center md:order-2">
                     {
                         auth() ?
@@ -55,8 +56,9 @@ export const Navbar = () => {
                     {
                         !auth() ?
                             <div>
-                                <Link to={"/login"}>Login</Link>
-                                <Link to={"/registration"}>Register</Link>
+                                <Link className="text-white bg-accent-dark hover:bg-accent focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" to={"/login"}>Login</Link>
+                                {/* <Link className="bg-accent-dark text-white py-2.5 px-5 mr-2 mb-2 text-sm font-medium mx-10 rounded-lg drop-shadow hover:bg-accent transition-colors duration-200" to={"/login"}>Login</Link> */}
+                                <Link className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-accent focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" to={"/registration"}>Register</Link>
                             </div> :
                             <div>
                                 <button onClick={() => handleSignOut()}>Sign Out</button>
@@ -89,7 +91,7 @@ export const Navbar = () => {
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                     </button>
                 </div>
-                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
+                <div className="mx-auto items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <Link to={"/"} className={`block py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent hover:text-accent md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 
