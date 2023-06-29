ca011944251cbd6bc550c3ac38e4d0e581fff452
radwija <115165737+radwija@users.noreply.github.com>
2023-06-29 23:04:13 +0800

    fixing switching dropdown responsive navbar buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,22 @@ export const Navbar = () => {
     const navigate = useNavigate();
     const userEmail = auth()?.email;
     const location = useLocation();
-    const [isExpanded, setExpanded] = React.useState(false);
+    const [isProfileExpanded, setProfileExpanded] = React.useState(false);
+    const [isBurgerExpanded, setBurgerExpanded] = React.useState(false);
+
+    const handleBurgerExpand = () => {
+        if (isProfileExpanded === true) {
+            setProfileExpanded(false)
+        }
+        setBurgerExpanded(!isBurgerExpanded)
+    }
+
+    const handleProfileExpand = () => {
+        if (isBurgerExpanded === true) {
+            setBurgerExpanded(false)
+        }
+        setProfileExpanded(!isProfileExpanded)
+    }
 
     const [profile, setProfile] = useState({});
     const handleSignOut = () => {
@@ -40,24 +55,6 @@ export const Navbar = () => {
         // Profile Toggle
     }, []);
 
-    const options = {
-        onCollapse: () => {
-            console.log('element has been collapsed')
-        },
-        onExpand: () => {
-            console.log('element has been expanded')
-        },
-        onToggle: () => {
-            console.log('element has been toggled')
-        }
-    };
-
-    // Navbar Toggle
-    const $targetEl = document.getElementById('mobile-menu-2');
-    const $triggerEl = document.getElementById('triggerEl');
-
-    const collapseNavbar = new Collapse($targetEl, $triggerEl, options);
-
     return (
 
         <nav className="fixed mx-auto w-full top-0 z-20 bg-white border-b border-gray-200">
@@ -75,7 +72,7 @@ export const Navbar = () => {
                             :
                             <div className="md:flex  items-center">
                                 <span className="hidden md:flex sm:hidden capitalize mr-3">{roleName}</span>
-                                <button onClick={() => setExpanded(!isExpanded)} id="user-menu-button" type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                                <button onClick={() => handleProfileExpand()} id="user-menu-button" type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                                     <span className="sr-only">Open user menu</span>
                                     <div className="w-8 h-8">
                                         {profile?.picture ? (
@@ -107,8 +104,8 @@ export const Navbar = () => {
                     }
                     {/* <!-- Dropdown menu --> */}
                     {
-                        auth() && isExpanded ?
-                            <div id="user-dropdown" className="z-50 absolute top-12 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" >
+                        auth() && isProfileExpanded ?
+                            <div id="user-dropdown" className="z-50 absolute right-12 top-12 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" >
                                 <div className="px-4 py-3">
                                     <span className="block text-sm text-gray-900 dark:text-white">{profile?.name}</span>
                                     <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">{userEmail}</span>
@@ -139,12 +136,12 @@ export const Navbar = () => {
                             </div>
                             : <></>
                     }
-                    <button id="triggerEl" data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <button onClick={() => handleBurgerExpand()} id="triggerEl" data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                     </button>
                 </div>
-                <div id="mobile-menu-2" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+                <div id="mobile-menu-2" className={`items-center justify-between ${isBurgerExpanded === true ? '' : 'hidden'} w-full md:flex md:w-auto md:order-1`}>
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <Link to={"/"} className={`block py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent hover:text-accent md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 
@@ -184,7 +181,7 @@ export const Navbar = () => {
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav >
 
     )
 }
