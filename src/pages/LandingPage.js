d930566d43edaf418f392f7c4b17103e421c61bc
radwija <115165737+radwija@users.noreply.github.com>
2023-06-30 11:56:20 +0800

    hero section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,7 +4,17 @@ import Layout from '../components/Layout'
 export const LandingPage = () => {
     return (
         <Layout>
-            <div>LandingPage</div>
+            <section className='mt-10 sm:flex sm:flex-wrap sm:flex-col md:grid md:grid-cols-12 p-5 '>
+                <div className='lg:px-20 md:px-8 py-5 text-center lg:text-left md:text-left sm:text-center sm:col-span-12 md:col-span-6'>
+                    <h1 className='text-4xl md:text-6xl font-semibold mb-6'>Meals on Wheels</h1>
+                    <p className='w-full'>
+                        Bringing Comfort and Nourishment to Those in Need: Join us in our mission to deliver hot, nutritious meals to individuals facing age, disease, or disability through the Meals On Wheels program.
+                    </p>
+                </div>
+                <div className='lg:px-20 md:px-8 py-5 sm:col-span-12 md:col-span-6'>
+                    <img src="https://images.pexels.com/photos/6646768/pexels-photo-6646768.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="https://images.pexels.com/photos/6646768/pexels-photo-6646768.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+                </div>
+            </section>
         </Layout>
     )
 }
