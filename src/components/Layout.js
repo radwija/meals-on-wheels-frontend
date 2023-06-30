300751a49de4ee0dbdb619d4ef88bae6ac4355f7
aguswirayasa <aguswirayasa08@gmail.com>
2023-06-30 13:16:06 +0800

    removing unuse import and improving update profile

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,16 +3,16 @@ import { Footer } from "./Footer";
 import { Outlet } from "react-router";
 import { LandingPage } from "../pages/LandingPage";
 import { Navbar } from "./Navbar";
-const Layout = (props) => {
+const Layout = ({ children, isUpdated }) => {
   return (
     <div>
-      <Navbar />
+      <Navbar isUpdated={isUpdated} />
       {/* <Outlet /> */}
       <div className="min-h-screen md:pt-[65px] sm:pt-[73px] pt-[73px]">
-        {props.children}
+        {children}
       </div>
       <Footer />
-    </div >
+    </div>
   );
 };
 
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -309,6 +309,7 @@ const Registration = () => {
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="confirmPassword"
+                  placeholder="Confirm Password"
                   type="password"
                   value={values.confirmPassword}
                   onChange={handleChange}
